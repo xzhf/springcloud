f32224ae8e30cc374cf9fbca21adaa6f3fb436b8
xzhf <1071033489@qq.com>
2018-04-16 18:03:50 +0800

    ...

--- xx-springcloud-config-server/src/main/java/com/xx/springcloud/config/server/mysql/entity/ConfigRepository.java
+++ xx-springcloud-config-server/src/main/java/com/xx/springcloud/config/server/mysql/entity/ConfigRepository.java
@@ -1,10 +1,4 @@
 package com.xx.springcloud.config.server.mysql.entity;
-/******************************************************************************
- * Copyright (C) 2017 ShenZhen ComTop Information Technology Co.,Ltd
- * All Rights Reserved.
- * 本软件为深圳康拓普开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、
- * 复制、修改或发布本软件.
- ******************************************************************************/
 
 import javax.persistence.*;
 import java.util.ArrayList;
--- xx-springcloud-config-server/src/main/java/com/xx/springcloud/config/server/mysql/service/ConfigRepositoryService.java
+++ xx-springcloud-config-server/src/main/java/com/xx/springcloud/config/server/mysql/service/ConfigRepositoryService.java
@@ -1,10 +1,5 @@
 package com.xx.springcloud.config.server.mysql.service;
-/******************************************************************************
- * Copyright (C) 2017 ShenZhen ComTop Information Technology Co.,Ltd
- * All Rights Reserved.
- * 本软件为深圳康拓普开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、
- * 复制、修改或发布本软件.
- ******************************************************************************/
+
 
 import com.xx.springcloud.config.server.mysql.entity.ConfigRepository;
 import com.xx.springcloud.config.server.mysql.repository.ConfigDBRepository;
