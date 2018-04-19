5d411051da83af807888d83305822201ca14cb52
xzhf <1071033489@qq.com>
2018-04-19 16:14:44 +0800

    eureka

--- xx-springcloud-config-server/src/main/java/com/xx/springcloud/config/server/SpringcloudConfigServerApplication.java
+++ xx-springcloud-config-server/src/main/java/com/xx/springcloud/config/server/SpringcloudConfigServerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class SpringcloudConfigServerApplication extends SpringBootServletInitializer {
