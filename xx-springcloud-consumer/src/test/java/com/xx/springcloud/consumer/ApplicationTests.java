a105d4e3178049215ac4e701c6d0ea43053c5544
xzhf <1071033489@qq.com>
2018-04-19 16:23:00 +0800

    ...

--- xx-springcloud-consumer/src/test/java/com/xx/springcloud/consumer/ApplicationTests.java
+++ xx-springcloud-consumer/src/test/java/com/xx/springcloud/consumer/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.xx.springcloud.provider;
+package com.xx.springcloud.consumer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class XxSpringcloudProviderApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
