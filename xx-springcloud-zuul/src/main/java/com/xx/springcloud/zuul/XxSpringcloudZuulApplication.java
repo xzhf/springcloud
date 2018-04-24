d4638183ec7985d170bf6c1ed30c9e0ac78102cd
xzhf <1071033489@qq.com>
2018-04-24 18:01:25 +0800

    zuul

---
+++ xx-springcloud-zuul/src/main/java/com/xx/springcloud/zuul/XxSpringcloudZuulApplication.java
@@ -0,0 +1,16 @@
+package com.xx.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class XxSpringcloudZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxSpringcloudZuulApplication.class, args);
+	}
+}
