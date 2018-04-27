7e1c1b08b90ef949cf7a807181a4864e51381de0
xzhf <1071033489@qq.com>
2018-04-27 10:14:35 +0800

    整合sleuth

--- xx-springcloud-provider/src/main/java/com/xx/springcloud/provider/ProviderApplication.java
+++ xx-springcloud-provider/src/main/java/com/xx/springcloud/provider/ProviderApplication.java
@@ -3,6 +3,8 @@ package com.xx.springcloud.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -11,4 +13,9 @@ public class ProviderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProviderApplication.class, args);
 	}
+
+    @Bean
+    public AlwaysSampler defaultSampler() {
+        return new AlwaysSampler();
+    }
 }
