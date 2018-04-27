1f4618a45507433fa5e71d50e6973b9f6f20c9c6
xzhf <1071033489@qq.com>
2018-04-27 10:27:26 +0800

    ..

--- xx-springcloud-bus/src/main/java/com/xx/springcloud/bus/component/MsgProducer.java
+++ xx-springcloud-bus/src/main/java/com/xx/springcloud/bus/component/MsgProducer.java
@@ -1,11 +1,11 @@
 package com.xx.springcloud.bus.component;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 /**
+ * 消息生产者
  * @author xuzhifan
  * @version 1.0
  * @since 2018/4/26
@@ -16,8 +16,8 @@ public class MsgProducer {
     @Autowired
     private KafkaTemplate kafkaTemplate;
 
+
     public void sendMsg(Object obj) {
-//        JSONObject.toJSONString(obj)
         kafkaTemplate.sendDefault(obj);
     }
 }
