9fee454859ad8b337bf1a347034df7138da2f9f8
xzhf <1071033489@qq.com>
2018-04-27 10:24:50 +0800

    整合sleuth

--- xx-springcloud-consumer/src/main/java/com/xx/springcloud/consumer/Application.java
+++ xx-springcloud-consumer/src/main/java/com/xx/springcloud/consumer/Application.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +20,11 @@ public class Application {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler() {
+        return new AlwaysSampler();
     }
 
     public static void main(String[] args) {
