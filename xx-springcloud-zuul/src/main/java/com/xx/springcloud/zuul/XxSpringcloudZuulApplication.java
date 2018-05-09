c2f4037511a3a7c612ceaa92818c665a4b839103
xzhf <1071033489@qq.com>
2018-05-09 18:02:06 +0800

    hystrix dashboard

--- xx-springcloud-zuul/src/main/java/com/xx/springcloud/zuul/XxSpringcloudZuulApplication.java
+++ xx-springcloud-zuul/src/main/java/com/xx/springcloud/zuul/XxSpringcloudZuulApplication.java
@@ -3,10 +3,12 @@ package com.xx.springcloud.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 
+@EnableHystrixDashboard
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
---
+++ xx-springcloud-zuul/src/test/java/com/xx/springcloud/zuul/XxSpringcloudZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xx.springcloud.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XxSpringcloudZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
